c28bdcec7dbfad34f6b6d29c904faffe9f170517
kokoriru <kokorirury@yahoo.co.jp>
2016-03-28 17:44:00 +0900

    model の関係を追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,9 @@
 class Book < ActiveRecord::Base
   scope :costly, -> { where("price > ?", 3000) }
   scope :written_about, -> { where("name like ?", "%#{theme}%") }
-  default_scope -> { order("published_on desc") }
+
+  belongs_to :publisher
+
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 end
