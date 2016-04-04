4a128c32348b3e1b3405acf2619c8b08869f5270
kokoriru <kokorirury@yahoo.co.jp>
2016-04-04 11:30:38 +0900

    Book ページに update 機能を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :set_publisher_id
+
   def show
     respond_to do |format|
       format.html
@@ -10,9 +12,25 @@ class BooksController < ApplicationController
   def edit
   end
 
+  def update
+    if @book.update(book_params)
+      redirect_to publisher_book_path, notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def set_book
     @book = Book.find(params[:id])
+  end
+
+  def set_publisher_id
+    @publisher = Publisher.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:id, :name, :price, :publisher_id)
   end
 end
